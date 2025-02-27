259dc3a83ae5bc9f32fe173eb2b23557bae1f8a6
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-28 06:28:45 +0900

    feat: 댓글 기능 추가 및 레이아웃 수정

--- js/index.js
+++ js/index.js
@@ -17,3 +17,34 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const commentInput = document.querySelector("#comment-2 textarea");
+  const submitButton = document.querySelector("#post_button");
+  const commentList = document.querySelector(".comment-list");
+  console.log(submitButton)
+
+  submitButton.addEventListener("click", function () {
+    const commentText = commentInput.value.trim();
+
+    if (commentText === "") {
+      alert("댓글을 입력해주세요.");
+      return;
+    }
+
+    const newComment = document.createElement("li");
+    newComment.classList.add("comment-item");
+    newComment.innerHTML = `
+      <div class="comment-item">
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">${commentText}</div>
+      </div>
+    `;
+
+    commentList.appendChild(newComment);
+    commentInput.value = "";
+  });
+});
